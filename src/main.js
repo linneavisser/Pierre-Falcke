6e5dc9c816fd9783596f6d4d8d3d1de019809754
Nina Vergmann <nina152b@kea.stud.dk>
2022-09-12 13:40:35 +0200

    lastest changes

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
 import "./style.css";
 /* Import af Motion One bibliotek */
 import { animate, stagger, inView, scroll, timeline } from "motion";
+
+document.querySelectorAll(".parallaxcontainer").forEach((parallaxcontainer) => {
+  const elementderskalparallaxes = parallaxcontainer.querySelector("p");
+  scroll(animate(elementderskalparallaxes, { y: [50, 500] }), {
+    target: elementderskalparallaxes,
+  });
+});
+
+document
+  .querySelectorAll(".parallaxcontainermedflereelementer")
+  .forEach((parallaxcontainer) => {
+    const elementderskalparallaxes2 =
+      parallaxcontainer.querySelector(".paragraf2");
+    scroll(animate(elementderskalparallaxes2, { y: [0, 800] }), {
+      target: elementderskalparallaxes2,
+    });
+  });
