38206e180d699c8797c9a29c53fb80462a463dfe
Linnea Visser <anna56c1@stud.kea.dk>
2022-09-16 12:50:58 +0200

    hehej

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        'pierre-bg-foto': "url('/src/foto-pierre-falcke.webp')",
+      }
     },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
